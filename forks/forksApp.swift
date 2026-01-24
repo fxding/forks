08e1298a412c4bf9365bc711d0b9dd8e3de07899
Fuxian Ding <nekleding@gmail.com>
2026-01-24 21:40:22 +0800

    Initial Commit

---
+++ forks/forksApp.swift
@@ -0,0 +1,17 @@
+//
+//  forksApp.swift
+//  forks
+//
+//  Created by Fuxian on 2026/1/24.
+//
+
+import SwiftUI
+
+@main
+struct forksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
